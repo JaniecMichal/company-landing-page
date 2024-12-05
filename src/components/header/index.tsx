7c5b5f2710757591d8de987dcdf67b7ad0905b29
MichalJaniec <michal.janiec95@gmail.com>
2024-12-05 18:11:03 +0100

    adding logo to header

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,10 +1,15 @@
+import Image from "next/image";
+
 import { Navbar } from "../navbar";
 import { Socials } from "../socials";
+
+import logo from "../../assets/logo.png";
 
 export const Header = () => {
 	return (
 		<header className="flex flex-col items-center justify-between px-8 py-6 md:flex-row">
 			<Navbar />
+			<Image src={logo} width={80} height={80} alt="company-logo" />
 			<Socials />
 		</header>
 	);
