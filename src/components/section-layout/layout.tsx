b59a8a926959ac1f5da44224ce259d98968c3e4e
MichalJaniec <michal.janiec95@gmail.com>
2024-12-05 23:01:01 +0100

    impr spacing between sections

--- src/components/section-layout/layout.tsx
+++ src/components/section-layout/layout.tsx
@@ -7,7 +7,7 @@ type SectionLayoutProps = {
 
 export const SectionLayout = ({ sectionTitle, children }: SectionLayoutProps) => {
 	return (
-		<article>
+		<article className="mb-12">
 			<SectionHeader title={sectionTitle} />
 			<div className="container mx-auto px-4 md:px-6 lg:px-8">{children}</div>
 		</article>
