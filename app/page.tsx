ed036f4bdda3f0babac7740b54a6a03e5a3f0c13
MichalJaniec <michal.janiec95@gmail.com>
2024-12-05 22:30:15 +0100

    add services to page

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,9 @@
-import { MainHeading, SubHeading } from "@/components/headings";
+import { Services } from "@/components/services";
 
 export default async function Home() {
 	return (
-		<section className="h-52 bg-slate-600 py-12 pt-12">
-			<div className="container mx-auto px-4 md:px-6 lg:px-8">
-				<MainHeading text="Michal Janiec" />
-			</div>
+		<section className="py-12">
+			<Services />
 		</section>
 	);
 }
